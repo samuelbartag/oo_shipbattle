23dcddf2df0536620b7ee45c827c50852c2e8d9a
samuelbartag <samuel@samuelbartag.com.br>
2018-03-08 22:11:58 -0300

    oo-ep2 - Chapter 9

--- lib/ShipLoader.php
+++ lib/ShipLoader.php
@@ -8,10 +8,25 @@
 
 class ShipLoader
 {
+    private $pdo;
+
+    private $dbDsn;
+
+    private $dbUser;
+
+    private $dbPass;
+
+    public function __construct($dbDsn, $dbUser, $dbPass)
+    {
+        $this->dbDsn = $dbDsn;
+        $this->dbUser = $dbUser;
+        $this->dbPass = $dbPass;
+    }
+
     /**
      * @return Ship[]
      */
-    function getShips()
+    public function getShips()
     {
         $shipsData= $this->queryForShips();
 
@@ -29,8 +44,7 @@ class ShipLoader
      */
     public function findOneById($id)
     {
-        $pdo = new PDO('mysql:host=172.17.0.2;dbname=oo_battle', 'root', 'passwd');
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo = $this->getPDO();
         $statement = $pdo->prepare('SELECT * FROM ship WHERE id = :id');
         $statement->execute(array('id' => $id));
         $shipArray = $statement->fetch(PDO::FETCH_ASSOC);
@@ -55,12 +69,26 @@ class ShipLoader
 
     private function queryForShips()
     {
-        $pdo = new PDO('mysql:host=172.17.0.2;dbname=oo_battle', 'root', 'passwd');
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo = $this->getPDO();
         $statement = $pdo->prepare('SELECT * FROM ship');
         $statement->execute();
         $shipsArray = $statement->fetchAll(PDO::FETCH_ASSOC);
 
         return $shipsArray;
+    }
+
+    /**
+     * @return PDO
+     */
+    private function getPDO()
+    {
+        if($this->pdo === null) {
+            $pdo = new PDO($this->dbDsn, $this->dbUser, $this->dbPass);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $this->pdo = $pdo;
+        }
+
+        return $this->pdo;
     }
 }
